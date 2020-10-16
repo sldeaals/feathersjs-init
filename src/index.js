01f85fded01c18f510d3c431fb089f15f46c0eec
sslldealllss <danielariasaguero@gmail.com>
2020-10-16 17:20:56 +0300

    Cluster moved to index.js for cleaner code reasons

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 /* eslint-disable no-console */
+const cluster = require('cluster');
 const logger = require('./logger');
-const app = require('./app');
+// Count the machine's CPUs
+const clusterCount = require('os').cpus().length;
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
+const workers = [];
 
-if (app.get){
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  
+  // Create a worker for each CPU
+  for (let i = 0; i < clusterCount; i += 1) {
+    let worker = cluster.fork(); 
+    //listen message from worker
+    worker.on('message', function(data) {
+      //Broadcast the message
+      for (let j in workers) {workers[j].send(data);}
+    });
+    //Add worker to array
+    workers.push(worker);
+  }
+}
+
+if (cluster.isWorker) {
+  console.log(`Worker ${process.pid} started`);
+
+  const app = require('./app');
+  
+  process.on('unhandledRejection', (reason, p) =>
+    logger.error('Unhandled Rejection at: Promise ', p, reason)
+  );
+
   const port = app.get('port');
   const server = app.listen(port);
   server.on('listening', () =>
     logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
   );
-  console.log(`Worker ${process.pid} started`);
 }
